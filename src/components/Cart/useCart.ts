96a3b2d9711defbe10cc8a0e35cdfc25caff9e61
Nick Spaargaren <nickspaargaren@users.noreply.github.com>
2024-09-13 23:20:51 +0200

    Add persist storage to cart

--- src/components/Cart/useCart.ts
+++ src/components/Cart/useCart.ts
@@ -1,4 +1,5 @@
 import { create } from "zustand";
+import { persist, createJSONStorage } from "zustand/middleware";
 
 interface State {
   amount: number;
@@ -8,7 +9,15 @@ interface Action {
   updateAmount: (amount: State["amount"]) => void;
 }
 
-export const useCart = create<State & Action>((set) => ({
-  amount: 0,
-  updateAmount: (amount) => set(() => ({ amount: amount })),
-}));
+export const useCart = create<State & Action>()(
+  persist(
+    (set) => ({
+      amount: 0,
+      updateAmount: (amount) => set({ amount }),
+    }),
+    {
+      name: "cart",
+      storage: createJSONStorage(() => sessionStorage),
+    },
+  ),
+);
